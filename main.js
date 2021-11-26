b58971ebcda9f527ce7f08ac440959c5f6e9e45a
joellat <81321308+joellat@users.noreply.github.com>
2021-11-26 02:22:42 +0100

    25/11 night finished

--- main.js
+++ main.js
@@ -74,6 +74,7 @@ function reply_click(clicked_id) {
     currentCaseArticle.classList.add("caseArticleOpen");
   }
 }
+
 function button_click(intro_id) {
   let currentlyClicked = document.getElementById(intro_id);
   let currentlyOpen = document.getElementById(intro_id + "open");
@@ -100,57 +101,6 @@ function button_click(intro_id) {
   }
 }
 
-// function button_click(intro_id) {
-//   if (
-//     document
-//       .getElementById(intro_id)
-//       .classList.contains("introductionButtonClicked")
-//   ) {
-//     document
-//       .getElementById(intro_id)
-//       .classList.remove("introductionButtonClicked");
-//     document.getElementById("introductionLine").style.paddingRight = "13vw";
-//     let buttonElementOpen = document.getElementById(intro_id + "open");
-//     buttonElementOpen.style.display = "none";
-//     document.getElementById(intro_id).style.borderColor = boneColor;
-//     document.getElementById(intro_id).style.color = boneColor;
-//     // document.getElementById(intro_id).hover.style.color = orangeColor;
-//     // document.getElementById(intro_id).hover.style.borderColor = orangeColor;
-//   } else {
-//     // for (i = 0; i < 5; i++) {
-//     //   if (
-//     //     introductionArray[i].classList.contains("introductionButtonClicked")
-//     //   ) {
-//     //     introductionArray[i].classList.remove("introductionButtonClicked");
-//     //     console.log(introductionArray[i].id);
-//     //     let buttonElementOpen = introductionArray[i].id + "open";
-//     //     buttonElementOpen.style.display = "none";
-//     //     document.getElementById(introductionArray[i].id).style.borderColor =
-//     //       boneColor;
-//     //     document.getElementById(introductionArray[i].id).style.color =
-//     //       boneColor;
-//     //   } else {
-//     //     continue;
-//     //   }
-//     // }
-//     for (i = 0; i < 5; i++) {
-//       if (
-//         introductionArray[i].classList.contains("introductionButtonClicked")
-//       ) {
-//       }
-//     }
-//     let buttonElementOpen = document.getElementById(intro_id + "open");
-//     console.log(buttonElementOpen);
-//     document.getElementById("introductionLine").style.paddingRight = "8vw";
-//     document
-//       .getElementById(intro_id)
-//       .classList.add("introductionButtonClicked");
-//     buttonElementOpen.style.display = "block";
-//     document.getElementById(intro_id).style.borderColor = orangeColor;
-//     document.getElementById(intro_id).style.color = orangeColor;
-//   }
-// }
-
 function pauseVideo() {
   if (
     document.body.scrollTop > 140 ||
@@ -165,125 +115,6 @@ function pauseVideo() {
   }
 }
 
-function casesOpacity() {
-  let windowWidth = window.innerWidth;
-  let caseWidth = (windowWidth / 10) * 3;
-  console.log(document.documentElement.scrollTop);
-  console.log(
-    document.getElementById("headImageClass").clientHeight + caseWidth
-  );
-  if (
-    document.documentElement.scrollTop >=
-    document.getElementById("headImageClass").clientHeight
-  ) {
-    document.getElementById("caseOneID").classList.add("caseContainerVisible");
-  }
-  if (
-    document.documentElement.scrollTop >=
-    document.getElementById("headImageClass").clientHeight + caseWidth
-  ) {
-    document.getElementById("caseTwoID").classList.add("caseContainerVisible");
-  }
-  if (
-    document.documentElement.scrollTop >=
-    document.getElementById("headImageClass").clientHeight + 2 * caseWidth
-  ) {
-    document
-      .getElementById("caseThreeID")
-      .classList.add("caseContainerVisible");
-  }
-  if (
-    document.documentElement.scrollTop >=
-    document.getElementById("headImageClass").clientHeight + 3 * caseWidth
-  ) {
-    document.getElementById("caseFourID").classList.add("caseContainerVisible");
-  }
-}
-
-// let windowHeight = window.innerHeight;
-// let videoHeight = document.getElementById("introductionVideo").clientHeight;
-// console.log(videoHeight);
-// console.log(windowHeight);
-// if (videoHeight > windowHeight) {
-//   let newMargin = videoHeight - windowHeight;
-//   console.log(newMargin);
-//   let newMarginTopBottom = newMargin / 2;
-//   console.log(newMarginTopBottom);
-//   document.getElementById("introductionVideo").style.marginTop =
-//     -newMargin + "px";
-// }
-
-// function newCircle() {
-//   let circleParent = document.getElementById("circleParent");
-//   let circleTwo = document.createElement("div");
-//   circleParent.appendChild(circleTwo);
-//   let circleSize = 0;
-//   if (lastScrollPosition - currentScrollPosition > 0) {
-//     circleSize -= 8;
-//   } else {
-//     circleSize += 8;
-//   }
-//   if (
-//     offset + maxPinkCircle <=
-//     document.documentElement.scrollTop + circleWidthHalf
-//   ) {
-//     if (circleSize < maxPinkCircle) {
-//       circleTwo.style.backgroundColor = "$pink-color";
-//       circleTwo.style.height = circleSize + "px";
-//       circleTwo.style.width = circleSize + "px";
-//     } else {
-//       newCircle();
-//     }
-//   }
-// }
-
-// let scrollCounter = 0;
-// let lastScrollPosition = document.documentElement.scrollTop;
-// let currentScrollPosition = document.documentElement.scrollTop;
-// console.log(currentScrollPosition);
-// console.log(lastScrollPosition);
-// let pinkCircleSize = 0;
-// let windowWidth = window.innerWidth;
-// let maxPinkCircle = (windowWidth / 10) * 4;
-// let circleWidthHalf = (windowWidth / 10) * 2;
-// let pinkCircle = document.getElementById("pinkCircle");
-// console.log(maxPinkCircle);
-// function checkWhiteCircle() {
-//   currentScrollPosition = document.documentElement.scrollTop;
-//   scrollCounter++;
-//   if (lastScrollPosition - currentScrollPosition > 0) {
-//     pinkCircleSize -= 8;
-//   } else {
-//     pinkCircleSize += 8;
-//   }
-//   if (
-//     offset <= document.body.scrollTop + circleWidthHalf ||
-//     offset <= document.documentElement.scrollTop + circleWidthHalf
-//   ) {
-//     if (pinkCircleSize < maxPinkCircle) {
-//       pinkCircle.style.height = pinkCircleSize + "px";
-//       pinkCircle.style.width = pinkCircleSize + "px";
-//     } else {
-//       newCircle();
-//     }
-//   } else {
-//     if (offset > document.documentElement.scrollTop - circleWidthHalf) {
-//       pinkCircle.style.height = "0px";
-//       pinkCircle.style.width = "0px";
-//       pinkCircleSize = 0;
-//     } else if (document.documentElement.scrollTop > offset + maxPinkCircle) {
-//       pinkCircle.style.height = maxPinkCircle + "px";
-//       pinkCircle.style.width = maxPinkCircle + "px";
-//     }
-//   }
-//   lastScrollPosition = document.documentElement.scrollTop;
-// }
 document.getElementById("introductionVideo").play();
 document.getElementById("case4Video").play();
 window.addEventListener("scroll", pauseVideo);
-window.addEventListener("scroll", casesOpacity);
-// window.addEventListener("scroll", checkWhiteCircle);
-// let bodyRect = document.getElementById("body").getBoundingClientRect();
-// let whiteRect = document.getElementById("whiteCircle").getBoundingClientRect();
-// let offset = whiteRect.top - bodyRect.top;
-// console.log(offset);
